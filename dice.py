8148b177b98729b82b515fed2b6a6714127ec41e
JefferyBentley <jeffery.bentley48@gmail.com>
2016-05-31 14:13:00 -0700

    changed function name

--- dice.py
+++ dice.py
@@ -2,13 +2,11 @@
 
 from random import randint
 
-def dice(number_of_dice, number_of_sides):
+def dices(number_of_dice, number_of_sides):
     for die in range(1, number_of_dice + 1):
         print("Die number " + str(die) + ":" + str(randint(1, number_of_sides)))
 
 dice_number = int(input("How many dice would you like to roll? "))
 dice_sides = int(input("How many sides sould each die have? "))
 
-dice(dice_number, dice_sides)
-
-
+dices(dice_number, dice_sides)
